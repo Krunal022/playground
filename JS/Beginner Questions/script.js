1b95cec16a44a182fdaa923e025cef53e5054c6c
Krunal022 <krunalvaghela723@gmail.com>
2026-06-13 19:10:54 +0530

    simple datatype-based questions

--- JS/Beginner Questions/script.js
+++ JS/Beginner Questions/script.js
@@ -54,3 +54,27 @@ let firstName = "Krunal";
 let lastName = "Waghela";
 let fullName = "Krunal Waghela!"
 console.log(`my firstName is ${firstName} and last name is ${lastName} or full name is ${fullName} `)
+
+// ---------- Data Types ----------
+
+// 1. Create variables of type string, number, boolean, null, and undefined.
+var str = "Krunal";
+var num = 240413149011;
+var bool = true;
+var nl = null;
+var undf = undefined;
+
+// 2. Check the type of different variables using `typeof`.
+console.log(typeof str, typeof num, typeof bool, typeof nl, typeof undf)
+
+// 3. Store your mobile number in a variable and check its type.
+var num = 240413149011;
+console.log(typeof num) // number
+
+// 4. Create a variable with value `null` and check its type.
+var nl = null;
+console.log(typeof nl) // object
+
+// 5. Create a bigint number and print it.
+var bigint = 125464215648848561321546n
+console.log(typeof bigint, bigint) // bigint 125464215648848561321546n
