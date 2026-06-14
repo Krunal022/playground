2fe0b459faf5ce4634d71d8fda83acc00b66f921
Krunal022 <krunalvaghela723@gmail.com>
2026-06-14 16:20:05 +0530

    Solve operators questions

--- JS/Beginner Questions/script.js
+++ JS/Beginner Questions/script.js
@@ -114,3 +114,52 @@ var a = "500px";
 var b = parseInt(a); // 500
 console.log(b)
 
+// ---------- Operators ---------- 
+
+// 1. Add two numbers and print the result.
+var a = 10;
+var b = 20;
+console.log(a + b) // 30
+
+// 2. Find the remainder when 25 is divided by 4.
+console.log(25 / 4) // 6.25
+console.log(25 % 4) // 1
+
+// 3. Find the square of a number using exponent operator.
+console.log(Math.pow(12, 2))
+
+// 4. Increment a variable using `++`.
+var a = 10
+console.log(++a) // 11
+
+// 5. Decrement a variable using `-`.
+var b = 50
+console.log(--b) // 49
+
+// 6. Use `+=` operator to increase a variable by 20.
+var c = 50;
+c += 20;
+console.log(c); // 70 
+
+// 7. Compare two numbers using `>`, `<`, `>=`, `<=`.
+var a = 70;
+var b = 40;
+console.log(a > b) // True
+console.log(a < b) // False
+console.log(a >= b) // True
+console.log(a <= b) // False
+
+// 8. Check if two values are strictly equal using `===`.
+console.log(a === b) // False
+
+// 9. Compare `"10"` and `10` using both `==` and `===`.
+console.log("10" == 10) // True
+console.log("10" === 10) // False
+
+// 10. Create two boolean variables and test `&&`, `||`, and `!`.
+var isStudent = true;
+var hasID = false;
+console.log(isStudent && hasID); // false
+console.log(isStudent || hasID); // true
+console.log(!isStudent);         // false
+console.log(!hasID);             // true
