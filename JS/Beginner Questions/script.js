016edd67fc2de860f8fb3485f5b576273438ac33
Krunal022 <krunalvaghela723@gmail.com>
2026-06-14 16:05:44 +0530

    simple Type Conversion & Coercion questions

--- JS/Beginner Questions/script.js
+++ JS/Beginner Questions/script.js
@@ -78,3 +78,39 @@ console.log(typeof nl) // object
 // 5. Create a bigint number and print it.
 var bigint = 125464215648848561321546n
 console.log(typeof bigint, bigint) // bigint 125464215648848561321546n
+
+// ---------- Type Conversion & Coercion ----------
+
+// 1. Convert the string `"50"` into a number.
+var a = "50"; // string
+var A = Number(a)
+console.log(typeof A) // number
+
+// 2. Convert the number `100` into a string.
+var num = 100; // number
+var str = String(num);
+console.log(typeof str)
+
+// 3. Convert `"true"` into a boolean.
+var a = "true" // string
+var b = Boolean(a);
+console.log(typeof b)
+
+// 4. Check the output of:
+// - `"5" + 2`
+// - `"5" - 2`
+// - `true + 1`
+console.log("5" + 2) // 52
+console.log("5" - 2) // 3
+console.log(true + 1) // 2
+
+// 5. Create a variable with value `"123abc"` and convert it into a number.
+var x = "123abc";
+var z = Number(x);
+console.log(z) // NaN
+
+// 6. Use `parseInt()` on `"500px"`.
+var a = "500px";
+var b = parseInt(a); // 500
+console.log(b)
+
