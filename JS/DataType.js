aed01e8f9dc0580ea22561cfabf78ed5206c9973
Krunal022 <krunalvaghela723@gmail.com>
2026-06-15 20:12:47 +0530

    simple datatype-based questions

---
+++ JS/DataType.js
@@ -0,0 +1,22 @@
+// 1. Create variables of type string, number, boolean, null, and undefined.
+var str = "Krunal";
+var num = 240413149011;
+var bool = true;
+var nl = null;
+var undf = undefined;
+
+// 2. Check the type of different variables using `typeof`.
+console.log(typeof str, typeof num, typeof bool, typeof nl, typeof undf)
+
+// 3. Store your mobile number in a variable and check its type.
+var num = 240413149011;
+console.log(typeof num) // number
+
+// 4. Create a variable with value `null` and check its type.
+var nl = null;
+console.log(typeof nl) // object
+
+// 5. Create a bigint number and print it.
+var bigint = 125464215648848561321546n
+console.log(typeof bigint, bigint) // bigint 125464215648848561321546n
+
