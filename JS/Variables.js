e87fb0b2ec1e0679463474298ed1e16241f36e5b
Krunal022 <krunalvaghela723@gmail.com>
2026-06-15 20:11:18 +0530

    variable-based question!

---
+++ JS/Variables.js
@@ -0,0 +1,37 @@
+// 1. Create a variable called `studentName` and store your name in it.
+var studentName = "Krunal W";
+console.log(studentName) // Krunal W
+
+// 2. Create a variable `age` and print it.
+var age = 26
+console.log("my age is " + age) // my age is 26
+
+// 3. Create two variables and swap their values.
+var a = 10;
+var b = 20;
+console.log(a, b) // 10 20
+
+temp = a;
+a = b;
+b = temp;
+console.log(a, b) // 20 10
+
+// 4. Create a constant variable for `PI` and print it.
+const PI = Math.PI;
+console.log(PI) //3.141592653589793
+
+// 5. Declare a variable without assigning a value and print it.
+var username;
+console.log(username) // undefined
+
+// 6. Create a variable `score` and increase it by 10.
+var score = 50;
+score += 10;
+console.log(score) // 60
+
+// 7. Create three variables for first name, last name, and full name.
+let firstName = "Krunal";
+let lastName = "Waghela";
+let fullName = "Krunal Waghela!"
+console.log(`my firstName is ${firstName} and last name is ${lastName} or full name is ${fullName} `)
+
