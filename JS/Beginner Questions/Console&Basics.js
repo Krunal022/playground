6faeaf3bd489eb10fb756a83af86fdf339142625
Krunal022 <krunalvaghela723@gmail.com>
2026-06-18 22:43:41 +0530

    basic JS console questions

---
+++ JS/Beginner Questions/Console&Basics.js
@@ -0,0 +1,15 @@
+// 1. Print `"Hello JavaScript"` in the console.
+console.log("Hello JavaScript");
+
+// 2. Print your name, age, and city using one `console.log()`.
+console.log("My name is krunal and i'm 22 y/o. from baroda!")
+
+// 3. Print a warning message using `console.warn()`.
+console.warn("This is the first warning to you dear!")
+
+// 4. Print an error message using `console.error()`.
+console.error("Here is my last error man!")
+
+// 5. Use `console.table()` to display an array of 5 numbers.
+console.table([10, 20, 30, 40, 50])
+console.table({ name: "krunal", age: 25, city: "vadodara" })
