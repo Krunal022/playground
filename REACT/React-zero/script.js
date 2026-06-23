b5a310895027f46cc2181133300eaf50290cfec3
Krunal022 <krunalvaghela723@gmail.com>
2026-06-23 20:22:32 +0530

    create lightweight React element and render!

--- REACT/React-zero/script.js
+++ REACT/React-zero/script.js
@@ -8,3 +8,21 @@ h1.textContent = "Hello from REAL DOM!";
 main.appendChild(h1)
 
 // This is the REAL DOM directly manipulated by the browser. The DOM is a tree-like structure that represents the HTML document, and it allows JavaScript to interact with and modify the content and structure of the webpage dynamically.
+
+// Check if React and ReactDOM are loaded
+console.log(React)
+console.log(ReactDOM)
+
+// Takes one element from the HTML document and creates a React root for it. This is where React will manage the rendering of components.
+let realDOMElement = document.querySelector('#root');
+
+// make it React root using ReactDOM.createRoot() method. 
+let rootOfReact = ReactDOM.createRoot(realDOMElement);
+
+// Create a React element using React.createElement() method. 
+// Thats lightweight and fast way to create React elements. 
+// React.createElement( type of element, containing any props, children of the element )
+let reactH1 = React.createElement("h1", {}, "Hello from React!")
+
+// Render the React element into the React root. 
+rootOfReact.render(reactH1)
